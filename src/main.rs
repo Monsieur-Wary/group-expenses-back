a585ba6bc7d7afc6135724309aa91cbb5a88c3cc
Thibault Dessarps <dessarps.thibault@gmail.com>
2020-09-09 23:56:23 +0200

    :recycle: using anyhow's result in main

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 #[actix_rt::main]
-async fn main() -> std::io::Result<()> {
+async fn main() -> anyhow::Result<()> {
     env_logger::from_env(env_logger::Env::default().default_filter_or("info")).init();
 
-    let configuration =
-        group_expenses::configuration::Settings::new().expect("Failed to read configuration.");
+    let configuration = group_expenses::configuration::Settings::new()?;
     let address = format!("127.0.0.1:{}", &configuration.application_port());
     let listener = std::net::TcpListener::bind(address)?;
-    group_expenses::startup::run(listener, configuration)?.await
+    group_expenses::startup::run(listener, configuration)?.await?;
+
+    Ok(())
 }
